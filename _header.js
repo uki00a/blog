70ada8baa04ab92159ef0056ce4d8e06015b6360
uki00a <uki00a@users.noreply.github.com>
2020-07-19 00:08:43 +0000

    deploy: 3f4d94359b3ff32fbe2d333a9217f2588cca6240

--- _header.js
+++ _header.js
@@ -1,5 +1,5 @@
 import { React } from "./_deps.js";
 const Header = () => (React.createElement("header", { className: "header" },
     React.createElement("div", null,
-        React.createElement("a", { href: "/" }, "TOP"))));
+        React.createElement("a", { href: "/blog" }, "TOP"))));
 export default Header;
