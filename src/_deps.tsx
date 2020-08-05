64b3364cb92386a802e00c2c5e5f297a4a6b2346
uki00a <uki00a@gmail.com>
2020-08-06 05:27:25 +0900

    fix: use denolib.com/Soremwar/deno_types instead of deno.land/x/types

--- src/_deps.tsx
+++ src/_deps.tsx
@@ -1,4 +1,4 @@
-// @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
+// @deno-types="https://denolib.com/Soremwar/deno_types/react/v16.13.1/react.d.ts"
 import React from "https://dev.jspm.io/react@16.13.1";
 export { PagicLayout } from "https://deno.land/x/pagic@v0.7.30/mod.ts";
 
