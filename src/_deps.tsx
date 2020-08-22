37e63cceb46a3b03ae893d31c1ca149e1b5d1340
uki00a <uki00a@gmail.com>
2020-08-22 11:03:31 +0900

    upgrade: bump pagic to v0.8.6

--- src/_deps.tsx
+++ src/_deps.tsx
@@ -1,5 +1,5 @@
 // @deno-types="https://denolib.com/Soremwar/deno_types/react/v16.13.1/react.d.ts"
 import React from "https://dev.jspm.io/react@16.13.1";
-export { PagicLayout } from "https://deno.land/x/pagic@v0.8.1/mod.ts";
+export { PagicLayout } from "https://deno.land/x/pagic@v0.8.6/mod.ts";
 
 export { React };
