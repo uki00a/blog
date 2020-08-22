a0ff52937912a35e7b5437439152f2e46d2fa5b9
uki00a <uki00a@gmail.com>
2020-08-22 12:39:20 +0900

    fix

--- src/_layout.tsx
+++ src/_layout.tsx
@@ -1,20 +1,36 @@
 import { React, PagicLayout } from "./_deps.tsx";
-import Header from "./_header.tsx";
 
-const Layout: PagicLayout = ({ title, content, publishedAt, updatedAt }) => (
-  <html>
-    <head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <link rel="stylesheet" href="assets/index.css" />
-    </head>
-    <body>
-      <Header />
-      <main>
-      {content}
-      </main>
-    </body>
-  </html>
-);
+function computeRelativePathToRootDir(outputPath: string): string {
+  const pathComponents = outputPath.split("/");
+  return "../".repeat(pathComponents.length - 1);
+}
+
+const Layout: PagicLayout = ({
+  outputPath,
+  title,
+  content,
+}) => {
+  const rootDir = computeRelativePathToRootDir(outputPath);
+  const linkToTopPage = `${rootDir}index.html`;
+  return (
+    <html>
+      <head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1/new.min.css"></link>
+      </head>
+      <body>
+        <header>
+          <div>
+            <a href={linkToTopPage}>タイトル</a>
+          </div>
+        </header>
+        <main>
+        {content}
+        </main>
+      </body>
+    </html>
+  );
+};
 
 export default Layout;
