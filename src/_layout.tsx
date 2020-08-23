1b93f2d32a27b1d6ee16e3c6bbf6fbcfc5d3f722
uki00a <uki00a@gmail.com>
2020-08-24 01:05:27 +0900

    chore: add favicon.ico

--- src/_layout.tsx
+++ src/_layout.tsx
@@ -12,12 +12,14 @@ const Layout: PagicLayout = ({
 }) => {
   const rootDir = computeRelativePathToRootDir(outputPath);
   const linkToTopPage = `${rootDir}index.html`;
+  const favicon = `${rootDir}assets/favicon.ico`;
   return (
     <html>
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1/new.min.css"></link>
+        <link rel="icon" href={favicon}></link>
       </head>
       <body>
         <header>
