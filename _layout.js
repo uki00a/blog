4e92f09d10a33b9ac7da629fc536f11a5b69b40b
uki00a <uki00a@users.noreply.github.com>
2020-08-23 16:07:14 +0000

    deploy: 1b93f2d32a27b1d6ee16e3c6bbf6fbcfc5d3f722

--- _layout.js
+++ _layout.js
@@ -6,11 +6,13 @@ function computeRelativePathToRootDir(outputPath) {
 const Layout = ({ outputPath, title, content, }) => {
     const rootDir = computeRelativePathToRootDir(outputPath);
     const linkToTopPage = `${rootDir}index.html`;
+    const favicon = `${rootDir}assets/favicon.ico`;
     return (React.createElement("html", null,
         React.createElement("head", null,
             React.createElement("title", null, title),
             React.createElement("meta", { charSet: "utf-8" }),
-            React.createElement("link", { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/@exampledev/new.css@1/new.min.css" })),
+            React.createElement("link", { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/@exampledev/new.css@1/new.min.css" }),
+            React.createElement("link", { rel: "icon", href: favicon })),
         React.createElement("body", null,
             React.createElement("header", null,
                 React.createElement("div", null,
