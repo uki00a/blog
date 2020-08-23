4fbf32370351a3cc52101228d6f13ba7832d199e
uki00a <uki00a@users.noreply.github.com>
2020-08-23 06:49:03 +0000

    deploy: fd71748fdbc61ba5eb9466f68821d65b9053844f

--- index_props.js
+++ index_props.js
@@ -6,7 +6,7 @@ export default {
     'outputPath': "index.html",
     'title': "uki00a.github.io",
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<h1>uki00a.github.io</h1>\n<ul>\n<li><a href="articles/001-about-this-blog.html">このブログについて</a></li>\n</ul>\n<h2 id="%E3%83%AA%E3%83%B3%E3%82%AF">リンク<a class="anchor" href="#%E3%83%AA%E3%83%B3%E3%82%AF">§</a></h2>\n<ul>\n<li><a href="https://github.com/uki00a/blog">ソースコード</a></li>\n</ul>'
+            __html: '<h1>uki00a.github.io</h1>\n<ul>\n<li><a href="articles/002-how-to-use-rhysd-dotfiles.html">rhysd/dotfilesを使ってdotfilesリポジトリの管理を楽にする</a></li>\n<li><a href="articles/001-about-this-blog.html">このブログについて</a></li>\n</ul>\n<h2 id="%E3%83%AA%E3%83%B3%E3%82%AF">リンク<a class="anchor" href="#%E3%83%AA%E3%83%B3%E3%82%AF">§</a></h2>\n<ul>\n<li><a href="https://github.com/uki00a/blog">ソースコード</a></li>\n</ul>'
         } }),
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { crossOrigin: "anonymous", src: "https://unpkg.com/react@16.13.1/umd/react.production.min.js" }),
