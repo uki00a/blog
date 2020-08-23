cfbe2d2ce611aa39f7bb0433ef24c088cc37c60f
uki00a <uki00a@gmail.com>
2020-08-24 08:46:46 +0900

    fix: invalid URL

--- src/_layout.tsx
+++ src/_layout.tsx
@@ -1,6 +1,6 @@
 import { React, PagicLayout } from "./_deps.tsx";
 
-const URL = "https://https://uki00a.github.io/blog"; // TODO ローカル環境では`http://localhost:<port>`を設定できるようにする
+const URL = "https://uki00a.github.io/blog"; // TODO ローカル環境では`http://localhost:<port>`を設定できるようにする
 const TOP_PAGE = "index.html";
 const DEFAULT_DESCRIPTION = "uki00a.github.io";
 const SITE_IMAGE = "avatar.png";
