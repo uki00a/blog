7aa1358ae00ee98afd93114527d7ec636d0c0f7b
uki00a <uki00a@users.noreply.github.com>
2020-08-23 23:44:33 +0000

    deploy: 35ebbd9d6f190c8d8d8c745ee98153ec373f3aa2

--- _layout.js
+++ _layout.js
@@ -1,16 +1,25 @@
 import { React } from "./_deps.js";
-function computeRelativePathToRootDir(outputPath) {
-    const pathComponents = outputPath.split("/");
-    return "../".repeat(pathComponents.length - 1);
-}
+const URL = "https://https://uki00a.github.io/blog"; // TODO ローカル環境では`http://localhost:<port>`を設定できるようにする
+const TOP_PAGE = "index.html";
+const DEFAULT_DESCRIPTION = "uki00a.github.io";
+const SITE_IMAGE = "avatar.png";
 const Layout = ({ outputPath, title, content, }) => {
-    const rootDir = computeRelativePathToRootDir(outputPath);
-    const linkToTopPage = `${rootDir}index.html`;
-    const favicon = `${rootDir}assets/favicon.ico`;
+    const linkToTopPage = `${URL}/${TOP_PAGE}`;
+    const favicon = `${URL}/assets/favicon.ico`;
+    const ogImage = `${URL}/assets/${SITE_IMAGE}`;
+    const ogType = outputPath.endsWith(TOP_PAGE) ? "website" : "article";
+    const ogDescription = DEFAULT_DESCRIPTION; // TODO ページごとにdescriptionを変える
     return (React.createElement("html", null,
         React.createElement("head", null,
             React.createElement("title", null, title),
             React.createElement("meta", { charSet: "utf-8" }),
+            React.createElement("meta", { property: "og:title", content: title }),
+            React.createElement("meta", { property: "og:description", content: ogDescription }),
+            React.createElement("meta", { property: "og:type", content: ogType }),
+            React.createElement("meta", { property: "og:image", content: ogImage }),
+            React.createElement("meta", { name: "twitter:card", content: "summary" }),
+            React.createElement("meta", { name: "twitter:site", content: "@uki00a" }),
+            React.createElement("meta", { name: "twitter:creator", content: "@uki00a" }),
             React.createElement("link", { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/@exampledev/new.css@1/new.min.css" }),
             React.createElement("link", { rel: "icon", href: favicon })),
         React.createElement("body", null,
