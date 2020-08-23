a454cb730d0a90484678192e6501940c93fd35b6
uki00a <uki00a@gmail.com>
2020-08-24 08:38:02 +0900

    chore: add ogp tags

--- src/_layout.tsx
+++ src/_layout.tsx
@@ -1,23 +1,33 @@
 import { React, PagicLayout } from "./_deps.tsx";
 
-function computeRelativePathToRootDir(outputPath: string): string {
-  const pathComponents = outputPath.split("/");
-  return "../".repeat(pathComponents.length - 1);
-}
+const URL = "https://https://uki00a.github.io/blog"; // FIXME
+const TOP_PAGE = "index.html";
+const DEFAULT_DESCRIPTION = "uki00a.github.io";
+const SITE_IMAGE = "avatar.png";
 
 const Layout: PagicLayout = ({
   outputPath,
   title,
   content,
 }) => {
-  const rootDir = computeRelativePathToRootDir(outputPath);
-  const linkToTopPage = `${rootDir}index.html`;
-  const favicon = `${rootDir}assets/favicon.ico`;
+  const linkToTopPage = `${URL}/${TOP_PAGE}`;
+  const favicon = `${URL}/assets/favicon.ico`;
+  const ogImage = `${URL}/assets/${SITE_IMAGE}`;
+  const ogType = outputPath.endsWith(TOP_PAGE) ? "website" : "article";
+  const ogDescription = DEFAULT_DESCRIPTION; // TODO ページごとにdescriptionを変える
+
   return (
     <html>
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <meta property="og:title" content={title}>
+        <meta property="og:description" content={ogDescription} />
+        <meta property="og:type" content={ogType} />
+        <meta property="og:image" content={ogImage} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@uki00a" />
+        <meta name="twitter:creator" content="@uki00a" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1/new.min.css"></link>
         <link rel="icon" href={favicon}></link>
       </head>
