b9cdcc40d87f0bfcdd51dcb2d9164995602bc99e
uki00a <uki00a@gmail.com>
2021-08-21 22:13:16 +0900

    feat: Support gtag.js

---
+++ src/components/Gtag.jsx
@@ -0,0 +1,15 @@
+export default function Gtag({ id }) {
+  return (
+    <>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${id}`} />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${id}');`,
+        }}
+      />
+    </>
+  );
+}
